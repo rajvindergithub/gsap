e861f9463c85809a182f622f254080a48ef21a9d
rajvindergithub <rajvindergithub@gmail.com>
2026-08-12 06:02:16 +0530

    update gsap landing page

--- gsap-landing-page/assets/js/script.js
+++ gsap-landing-page/assets/js/script.js
@@ -1,29 +1,216 @@
 console.log('Script loaded');
 
-if (typeof gsap !== 'undefined' && typeof ScrollTrigger !== 'undefined') {
-  gsap.registerPlugin(ScrollTrigger);
+var heroFoldAnimOne = document.querySelector('.heroFold_anim_one');
+var heroFoldAnimTwo = document.querySelector('.heroFold_anim_two');
 
-  gsap.from('.gsap-lab__eyebrow, .gsap-lab h2, .gsap-lab__description', {
-    y: 24,
+gsap.from(heroFoldAnimOne, {
+    duration: 1,
+    y: 50,
     opacity: 0,
-    duration: 0.8,
-    stagger: 0.15,
-    ease: 'power3.out',
-    scrollTrigger: {
-      trigger: '.gsap-lab',
-      start: 'top 80%'
-    }
-  });
+    ease: "power2.out",
+    delay: 0.5,
+    repeat: -1,
+    repeatDelay: 1, 
+     yoyo: true,
+       
+       filter: "drop-shadow(0px -100px 0px rgba(255,255,255,0.5))",
+    
+});
 
-  gsap.from('.gsap-lab__card', {
-    y: 30,
+gsap.from(heroFoldAnimTwo, {
+    duration: 1,
+    y: -50,
     opacity: 0,
-    duration: 0.9,
-    stagger: 0.2,
-    ease: 'power3.out',
+    ease: "power2.out",
+    delay: 0.5, 
+    repeat: -1, 
+    repeatDelay: 1,
+     yoyo: true,
+  
+       filter: "drop-shadow(0px 100px 0px rgba(255,255,255,0.5))",
+});
+
+
+
+var rightElems = document.querySelectorAll('.sec-fold-right');
+
+rightElems.forEach(function(elem) {
+
+//        console.log('Element:', elem);
+
+        elem.addEventListener('mouseenter', function() {
+          
+            gsap.to( elem.childNodes[3], {
+                duration: 0.5,
+                opacity: 1,
+                ease: "power2.out",
+                scale: 1
+            });
+
+
+        });
+
+        elem.addEventListener('mouseleave', function() {
+            
+            gsap.to( elem.childNodes[3], {
+                duration: 0.5,
+                opacity: 0,
+                ease: "power2.out",
+                scale: 1
+            });
+
+        });
+
+        elem.addEventListener('mousemove', function(e) {
+           
+//            console.log(e);
+
+            gsap.to( elem.childNodes[3], {
+                 x : e.x - elem.getBoundingClientRect().x - 70, 
+                  y : e.y - elem.getBoundingClientRect().y - 120, 
+                  ease: "power2.out",
+            });
+            
+
+        });
+
+});
+
+var page3center = document.querySelector('#page3 .page3-center');
+
+var videoHeroPage = document.querySelector('#page3 video');
+
+var navBar = document.querySelector('nav');
+
+var close_video = document.querySelector('.close_video');
+
+page3center.addEventListener('click', function(){
+
+            page3center.style.display = "none";
+            close_video.style.opacity = 1;
+            
+            videoHeroPage.play(); 
+            gsap.to( videoHeroPage, {
+                   transform: "scaleX(1) scaleY(1)",
+                   opacity : 1,
+                   borderRadius: 0,                   
+
+
+            } );   
+            
+        
+});
+
+videoHeroPage.addEventListener('click', function(){
+
+        page3center.style.display = "block";  
+         close_video.style.opacity = 0;
+
+     videoHeroPage.pause(); 
+            gsap.to( videoHeroPage, {
+                   transform: "scaleX(0.1) scaleY(0.85)",
+                   opacity : 0,
+                   borderRadius: "40px",                   
+            } ); 
+});
+
+
+var sections = document.querySelectorAll('.sec-right');
+
+
+sections.forEach(function(elem){
+     
+    elem.addEventListener("mouseenter", function(){
+       
+        elem.childNodes[3].style.opacity = 1; 
+        elem.childNodes[3].play(); 
+        
+    });
+    
+     elem.addEventListener("mouseleave", function(){
+       
+        elem.childNodes[3].style.opacity = 0; 
+        elem.childNodes[3].load(); 
+        
+    });
+    
+    
+    
+    
+});
+
+
+var getDigitalProductList =  document.querySelectorAll('.dpdp_s_m_list h4');
+
+console.log(getDigitalProductList);
+
+getDigitalProductList.forEach(function(eachElem){
+    
+//    console.log(eachElem);
+    
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.dpdp_s_m_div_sec .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
     scrollTrigger: {
-      trigger: '.gsap-lab',
-      start: 'top 70%'
+        trigger: ".dpdp_s_m_div_sec .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
     }
-  });
-}
+     
+    
+});
+
+gsap.from('.dpdp_s_m_div_third .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
+    scrollTrigger: {
+        trigger: ".dpdp_s_m_div_third .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
+    }
+     
+    
+});
+
+
+gsap.from('.dpdp_s_m_div_four .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
+    scrollTrigger: {
+        trigger: ".dpdp_s_m_div_four .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
+    }
+     
+    
+});
+
+
+
+
+
