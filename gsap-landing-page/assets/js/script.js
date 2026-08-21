73a8899038134860bd4a75d1afa19f4010af7fd0
rajvindergithub <rajvindergithub@gmail.com>
2026-08-21 11:35:20 +0530

    gsap landing page

--- gsap-landing-page/assets/js/script.js
+++ gsap-landing-page/assets/js/script.js
@@ -1,3 +1,70 @@
+ 
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.dpdp_s_m_div_sec .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
+    scrollTrigger: {
+        trigger: ".dpdp_s_m_div_sec .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
+    }
+     
+    
+});
+
+gsap.from('.dpdp_s_m_div_third .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
+    scrollTrigger: {
+        trigger: ".dpdp_s_m_div_third .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
+    }
+     
+    
+});
+
+
+gsap.from('.dpdp_s_m_div_four .dpdp_s_m_list h4', {
+    x: 0,
+    duration: 2,
+    stagger: {
+        amount:-0.5
+    },
+     ease: 'power2.out',
+    scrollTrigger: {
+        trigger: ".dpdp_s_m_div_four .dpdp_s_m_list",
+        scroller: "body",
+        markers: false,
+        start: "top 80%",
+        end: "top 10%", 
+        scrub: true
+    }
+     
+    
+});
+
+
+
+
+
+
 console.log('Script loaded');
 
 var heroFoldAnimOne = document.querySelector('.heroFold_anim_one');
@@ -114,35 +181,35 @@ videoHeroPage.addEventListener('click', function(){
             } ); 
 });
 
-
-var sections = document.querySelectorAll('.sec-right');
-
-
-sections.forEach(function(elem){
-     
-    elem.addEventListener("mouseenter", function(){
-       
-        elem.childNodes[3].style.opacity = 1; 
-        elem.childNodes[3].play(); 
-        
-    });
-    
-     elem.addEventListener("mouseleave", function(){
-       
-        elem.childNodes[3].style.opacity = 0; 
-        elem.childNodes[3].load(); 
-        
-    });
-    
-    
-    
-    
-});
+//
+//var sections = document.querySelectorAll('.fddf_r_block');
+//
+//
+//sections.forEach(function(elem){
+//     
+//    elem.addEventListener("mouseenter", function(){
+//       
+//        elem.childNodes[3].style.opacity = 1; 
+//        elem.childNodes[3].play(); 
+//        
+//    });
+//    
+//     elem.addEventListener("mouseleave", function(){
+//       
+//        elem.childNodes[3].style.opacity = 0; 
+//        elem.childNodes[3].load(); 
+//        
+//    });
+//    
+//    
+//    
+//    
+//});
 
 
 var getDigitalProductList =  document.querySelectorAll('.dpdp_s_m_list h4');
 
-console.log(getDigitalProductList);
+//console.log(getDigitalProductList);
 
 getDigitalProductList.forEach(function(eachElem){
     
@@ -150,65 +217,125 @@ getDigitalProductList.forEach(function(eachElem){
     
 });
 
+
+jQuery('.fddf_r_b_video').mouseenter(function(){
+    console.log('mouse enter');
+     
+});
+
+jQuery('.fddf_r_b_video').mouseleave(function(){
+    console.log('mouse leave');
+     
+});
+ 
+
+const box = document.querySelectorAll(".fddf_r_block");
+ 
+box.forEach(function(boxEach, index){
+    
+    
+    console.log('.fddf_r_line_'+[index], 'dss'); 
+    
+    gsap.set('.fddf_r_line_'+[index], {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '2px',
+    scaleX: 0,
+    transformOrigin: 'left',
+    backgroundColor: '#fff'
+});
+    
+     
+    boxEach.addEventListener("mouseenter", () => {
+        gsap.to('.fddf_r_line_'+[index], {
+            scaleX: 1,
+            duration: 1,
+            ease: "power2.out"
+        });
+    });
+
+    boxEach.addEventListener("mouseleave", () => {
+        gsap.to('.fddf_r_line_'+[index], {
+            scaleX: 0,
+            duration: 1,
+            ease: "power2.in"
+        });
+    });
+    
+});
+
+/* Second fold animation */
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.dpdp_s_m_div_sec .dpdp_s_m_list h4', {
-    x: 0,
-    duration: 2,
-    stagger: {
-        amount:-0.5
-    },
-     ease: 'power2.out',
+var thrFoldFirst = jQuery('.left-an-th-fold-first');
+var thrFoldFirstRight = jQuery('.right-an-th-fold-first');
+
+var thrFoldSecond = jQuery('.left-an-th-fold-second');
+var thrFoldSecondRight = jQuery('.right-an-th-fold-second');
+
+const tl = gsap.timeline({
     scrollTrigger: {
-        trigger: ".dpdp_s_m_div_sec .dpdp_s_m_list",
-        scroller: "body",
-        markers: false,
-        start: "top 80%",
-        end: "top 10%", 
-        scrub: true
+        trigger: "#layout-gride-thrid-fold",
+        start: "top 30%",
+        end: "top 90%",
+        repeat: -1, 
+        yoyo: true,
+ 
+        
     }
-     
+});
+
+tl.from(thrFoldFirst, {
+    x: -1500,
+    duration: 1,
+    ease: "power4.out",
     
 });
 
-gsap.from('.dpdp_s_m_div_third .dpdp_s_m_list h4', {
-    x: 0,
-    duration: 2,
-    stagger: {
-        amount:-0.5
-    },
-     ease: 'power2.out',
-    scrollTrigger: {
-        trigger: ".dpdp_s_m_div_third .dpdp_s_m_list",
-        scroller: "body",
-        markers: false,
-        start: "top 80%",
-        end: "top 10%", 
-        scrub: true
-    }
-     
+tl.from(thrFoldFirstRight, {
+    x: 1500,
+    duration: 1,
+    ease: "power4.out",
+    
+}, '-=1');
+
+tl.from(thrFoldSecond, {
+    x: -1500,
+    duration: 1,
+    ease: "power4.out",
     
 });
 
-
-gsap.from('.dpdp_s_m_div_four .dpdp_s_m_list h4', {
-    x: 0,
-    duration: 2,
-    stagger: {
-        amount:-0.5
-    },
-     ease: 'power2.out',
-    scrollTrigger: {
-        trigger: ".dpdp_s_m_div_four .dpdp_s_m_list",
-        scroller: "body",
-        markers: false,
-        start: "top 80%",
-        end: "top 10%", 
-        scrub: true
-    }
-     
+tl.from(thrFoldSecondRight, {
+    x: 1500,
+    duration: 1,
+    ease: "power4.out",
     
+}, '-=1');
+
+
+
+
+/* Second fold animation */
+
+jQuery('.fddf_r_block').on('mouseenter', function () {
+    var video = jQuery(this).find('video').get(0);
+
+    video.currentTime = 0; // Start from beginning
+    video.play();
 });
+
+jQuery('.fddf_r_block').on('mouseleave', function () {
+    var video = jQuery(this).find('video').get(0);
+
+    video.pause();
+    video.currentTime = 0; // Reset when mouse leaves
+});
+
+
+
 
 
 
